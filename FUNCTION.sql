fafff62699f09c778309a1bd8c603ec9c0169e62
Daria Ogulchanskaya <55715903+daria-ogulchanskaya@users.noreply.github.com>
2020-04-15 12:28:53 +0300

    Updated FUNCTION.

--- FUNCTION.sql
+++ FUNCTION.sql
@@ -8,14 +8,14 @@ BEGIN
 	 SELECT @PlantationFlowers = SUM([PlantationFlowers].[Amount])
 	 FROM [PlantationFlowers] 
 	 WHERE [PlantationFLowers].[PlantationId] = @PlantationId AND
-		   [PlantationFlowers].[FLowerId] = @FlowerId
+	       [PlantationFlowers].[FLowerId] = @FlowerId
 
 	 SELECT @BookedFlowers = SUM([SupplyFlowers].[Amount])
 	 FROM [SupplyFlowers]
 	 LEFT JOIN [Supply] ON [Supply].[PlantationId] = @PlantationId AND
-						   [Supply].[Id] = [SupplyFlowers].[SupplyId]
+			       [Supply].[Id] = [SupplyFlowers].[SupplyId]
 
 	 IF (@Amount <= @PlantationFlowers - @BookedFlowers)
-		RETURN 1
+	 	RETURN 1
 	 RETURN 0
 END
