f59577e5aef4052244cb09f6a5c00e7ca5f00572
daria-ogulchanskaya <55715903+daria-ogulchanskaya@users.noreply.github.com>
2020-04-15 12:24:48 +0300

    Added FUNCTION.

---
+++ FUNCTION.sql
@@ -0,0 +1,21 @@
+CREATE FUNCTION SupplyCanBeCompleted (@FlowerId int, @PlantationId int, @Amount int)
+RETURNS BIT
+AS
+BEGIN
+	 DECLARE @PlantationFlowers int;
+	 DECLARE @BookedFlowers int;
+
+	 SELECT @PlantationFlowers = SUM([PlantationFlowers].[Amount])
+	 FROM [PlantationFlowers] 
+	 WHERE [PlantationFLowers].[PlantationId] = @PlantationId AND
+		   [PlantationFlowers].[FLowerId] = @FlowerId
+
+	 SELECT @BookedFlowers = SUM([SupplyFlowers].[Amount])
+	 FROM [SupplyFlowers]
+	 LEFT JOIN [Supply] ON [Supply].[PlantationId] = @PlantationId AND
+						   [Supply].[Id] = [SupplyFlowers].[SupplyId]
+
+	 IF (@Amount <= @PlantationFlowers - @BookedFlowers)
+		RETURN 1
+	 RETURN 0
+END
